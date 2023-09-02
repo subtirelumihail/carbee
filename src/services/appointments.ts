3fa8779da4a348b1053bfe95bf06bca57b3c7874
Mihail <dazzzz2007@gmail.com>
2023-09-02 18:49:11 +0300

    Fix pagination size issue

--- src/services/appointments.ts
+++ src/services/appointments.ts
@@ -11,13 +11,38 @@ export type Paginate = {
   before?: string;
 };
 
+// [BUG]: There is a issue with the pagination:
+// - When we try to go the next page based on the after cursor, the first appointment
+// on the next page is the same with the last one from the pervious page
+// - Before the last page get an array with a size smaller than the queried size
+// [SOLUTION]: We remove the first appointment if the cursor is on page >= 2 and increase
+// the requested page size to 13 so we can always have 12 elements displayed.
+// We also force the hasNextPage to be false if the appointments array is smaller than the
+// selected size
+
+// TODO: Persist pagination in the URL query
+
 export const getAppointments = async (paginate: Paginate) => {
   const paginateQuery = Object.keys(paginate)
     .map((type) => `&${type}=${paginate[type as keyof typeof paginate]}`)
     .join("");
+
+  const size = paginate.after ? 13 : 12;
+
   try {
-    const res = await axios.get(`/api/appointments?size=12${paginateQuery}`);
+    const res = await axios.get(
+      `/api/appointments?size=${size}${paginateQuery}`
+    );
     const appointments = res.data?.edges.map((edge: EdgesType) => edge.node);
+
+    if (appointments.length < size && res?.data?.pageInfo) {
+      res.data.pageInfo.hasNextPage = false;
+    }
+
+    if (paginate.after) {
+      appointments.shift();
+    }
+
     return {
       appointments,
       pageInfo: res?.data?.pageInfo,
